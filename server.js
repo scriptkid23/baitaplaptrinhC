863b8448516393b2b6e07b1830d7151d3ab81a9c
hoando <hoando.dev@gmail.com>
2023-03-31 01:23:12 +0700

    node

--- server.js
+++ server.js
@@ -1,53 +1,23 @@
-const {fastify} = require("fastify");
-
-const { Telegraf } = require("telegraf");
-const { message } = require("telegraf/filters");
+const  { Telegraf }  =require("telegraf");
+const { message } =  require('telegraf/filters');
 require("dotenv").config();
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-let token = process.env.BOT_TOKEN;
-let chat_gpt_model = process.env.CHAT_GPT_MODEL;
-let chat_gpt_token = process.env.CHAT_GPT_TOKEN;
-let port = process.env.PORT;
+bot.on(message("text"), ctx => ctx.reply("Hello"));
 
-let baseURL = "https://api.openai.com/v1/chat/completions";
+// Start webhook via launch method (preferred)
+bot.launch({
+  webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: process.env.MY_HEROKU_URL,
 
-const start = async () => {
-  try {
-    const bot = new Telegraf(token);
-    const app = fastify();
-    let webhookURL = process.env.MY_HEROKU_URL;
+    // Port to listen on; e.g.: 8080
+    port: 8080,
 
-    const webhook = await bot.createWebhook({ domain: webhookURL + "/" + token });
-    app.post(bot.secretPathComponent(), (req, rep) => webhook(req.raw, rep.raw));
-    bot.on(message("text"), async (ctx) => {
-      let bodyContent = JSON.stringify({
-        model: chat_gpt_model,
-        messages: [{ role: "user", content: ctx.update.message.text }],
-      });
-      let headersList = {
-        Accept: "*/*",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${chat_gpt_token}`,
-      };
-      let reqOptions = {
-        url: baseURL,
-        method: "POST",
-        headers: headersList,
-        data: bodyContent,
-      };
-      await ctx.telegram.sendChatAction(ctx.message.chat.id, "typing");
-      let response = await axios.request(reqOptions);
-      await ctx.telegram.sendMessage(
-        ctx.message.chat.id,
-        response.data.choices.at(-1).message["content"]
-      );
-    });
+    // Optional path to listen for.
+    // `bot.secretPathComponent()` will be used by default
 
-    app.listen({ port: port });
-  } catch (err) {
-    console.log(err);
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-start();
+    // Optional secret to be sent back in a header for security.
+    // e.g.: `crypto.randomBytes(64).toString("hex")`
+  },
+});
