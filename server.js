c39f514add8b8c5359504674b20cc2d999639f46
hoando <hoando.dev@gmail.com>
2023-03-31 02:05:49 +0700

    e

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const {Telegraf} = require('telegraf')
-const express = require('express')
-const expressApp = express()
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const expressApp = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || 3000;
+expressApp.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 expressApp.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.hears(/./, (ctx) => ctx.reply('Hello'))
-bot.startPolling()
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.hears(/./, (ctx) => ctx.reply("Hello"));
+bot.startPolling();
