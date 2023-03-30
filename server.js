042910fea1850cce38515e0a90b6dadeb400ec5d
hoando <hoando.dev@gmail.com>
2023-03-31 01:53:51 +0700

    e

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const { Telegraf } = require("telegraf");
-const { message } = require("telegraf/filters");
-const express = require("express");
+const Telegraf = require('telegraf')
+const express = require('express')
+const expressApp = express()
 
-require("dotenv").config();
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
-bot.start((ctx) => ctx.reply('Welcome'))
-
-if(process.env.environment == "PRODUCTION"){
-  bot.launch({
-    webhook:{
-      
-        domain: process.env.MY_HEROKU_URL + '/' + process.env.BOT_TOKEN,// Your domain URL (where server code will be deployed)
-        hookPath: process.env.BOT_TOKEN,
-        port: process.env.PORT || 8000
-    }
-  }).then(() => {
-    console.info(`The bot ${bot.botInfo.username} is running on server`);
-  });
-} else { // if local use Long-polling
-  bot.launch().then(() => {
-    console.info(`The bot ${bot.botInfo.username} is running locally`);
-  });
-}
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.hears(/./, (ctx) => ctx.reply('Hello'))
+bot.startPolling()
