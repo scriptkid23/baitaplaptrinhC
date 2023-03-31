1302c6aba698b96f49e399d19b9112a4dbe98030
Hoan Do <sieunhannhenkutesn@gmail.com>
2023-03-31 15:05:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ bot.on(message("text"), async (ctx) => {
     headers: headersList,
     data: bodyContent,
   };
-  await ctx.telegram.sendChatAction(ctx.message.chat.id, "typing");
   
   try {
+    await ctx.telegram.sendChatAction(ctx.message.chat.id, "typing");
     let response = await axios.request(reqOptions);
     await ctx.telegram.sendMessage(
       ctx.message.chat.id,
-      response.data.choices.at(-1).message["content"]
+      response.data.choices[0].message["content"]
     );
   } catch (error) {
     await ctx.telegram.sendMessage(
